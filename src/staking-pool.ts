3b988a29f458df1793b670b62ae21a615f4caade
normadn <norman.saade@gmail.com>
2023-11-04 16:15:26 -1000

    wip list of deposit ids in pool

--- src/staking-pool.ts
+++ src/staking-pool.ts
@@ -1,24 +1,28 @@
 import { dataSource } from "@graphprotocol/graph-ts";
 import { DepositToPool as DepositToPoolEvent } from "../generated/templates/StakingPool/StakingPool";
-import { DepositToPool } from "../generated/schema";
+import { DepositToPool, Create } from "../generated/schema";
 
 export function handleDepositToPool(event: DepositToPoolEvent): void {
   let context = dataSource.context();
   let poolAddress = context.getBytes("poolAddress");
   let poolId = context.getBytes("poolId");
 
-  let entity = new DepositToPool(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.pool = poolId;
-  entity.amount = event.params.amount;
-  entity.depositer = event.params.depositer;
-  entity.nftId = event.params.id;
-  entity.poolAddress = poolAddress;
+  let depositId = event.transaction.hash.concatI32(event.logIndex.toI32());
+  let deposit = new DepositToPool(depositId);
+  deposit.pool = poolId;
+  deposit.amount = event.params.amount;
+  deposit.depositer = event.params.depositer;
+  deposit.nftId = event.params.id;
+  deposit.poolAddress = poolAddress;
+  deposit.blockNumber = event.block.number;
+  deposit.blockTimestamp = event.block.timestamp;
+  deposit.transactionHash = event.transaction.hash;
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  deposit.save();
 
-  entity.save();
+  let pool = Create.load(poolId);
+  if (pool == null) {
+    pool = new Create(poolId);
+  }
+  pool.deposits = depositId;
 }
